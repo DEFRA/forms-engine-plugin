1a49325907f9457fac2470ec8d1383a7669a3b27
David Stone <davidjamesstone@gmail.com>
2025-06-27 13:03:48 +0100

    Add shared client js file

--- src/client/javascripts/application.js
+++ src/client/javascripts/application.js
@@ -1,16 +1,3 @@
-import { initAllAutocomplete as initAllAutocompleteImp } from '~/src/client/javascripts/autocomplete.js'
-import { initFileUpload as initFileUploadImp } from '~/src/client/javascripts/file-upload.js'
-import { initAllGovuk as initAllGovukImp } from '~/src/client/javascripts/govuk.js'
-import { initPreviewCloseLink as initPreviewCloseLinkImp } from '~/src/client/javascripts/preview-close-link.js'
+import { initAll } from '~/src/client/javascripts/shared.js'
 
-export const initAllGovuk = initAllGovukImp
-export const initAllAutocomplete = initAllAutocompleteImp
-export const initFileUpload = initFileUploadImp
-export const initPreviewCloseLink = initPreviewCloseLinkImp
-
-export function initAll() {
-  initAllGovuk()
-  initAllAutocomplete()
-  initFileUpload()
-  initPreviewCloseLink()
-}
+initAll()
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,8 @@ export default {
   entry: {
     application: {
       import: ['./javascripts/application.js', './stylesheets/application.scss']
-    }
+    },
+    shared: './javascripts/shared.js'
   },
   experiments: {
     outputModule: true
