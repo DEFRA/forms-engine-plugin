b57542c7eb7f8da152114c822e79fe8f26b7bc0b
Alex Luckett <alexgluckett@gmail.com>
2025-08-08 14:24:23 +0100

    Temporarily comment out save and return demo to let tests pass

--- src/server/plugins/engine/configureEnginePlugin.ts
+++ src/server/plugins/engine/configureEnginePlugin.ts
@@ -2,7 +2,6 @@ import { join, parse } from 'node:path'
 
 import { type FormDefinition } from '@defra/forms-model'
 
-import { createLogger } from '~/src/server/common/helpers/logging/logger.js'
 import { FORM_PREFIX } from '~/src/server/constants.js'
 import { FormModel } from '~/src/server/plugins/engine/models/FormModel.js'
 import { plugin } from '~/src/server/plugins/engine/plugin.js'
@@ -12,8 +11,6 @@ import { type PluginOptions } from '~/src/server/plugins/engine/types.js'
 import { findPackageRoot } from '~/src/server/plugins/engine/vision.js'
 import { devtoolContext } from '~/src/server/plugins/nunjucks/context.js'
 import { type RouteConfig } from '~/src/server/types.js'
-
-const logger = createLogger()
 
 export const configureEnginePlugin = async ({
   formFileName,
@@ -62,50 +59,53 @@ export const configureEnginePlugin = async ({
       preparePageEventRequestOptions,
       onRequest,
       baseUrl: 'http://localhost:3009', // always runs locally
-      saveAndReturn: saveAndReturn ?? {
-        keyGenerator: (request) => {
-          if (!request.yar.id) {
-            throw new Error('No session ID found')
-          }
+      saveAndReturn
+      // Uncomment the following lines to enable save and return functionality
+      // This is not implemented in devtool mode, but can be used for testing purposes.
+      // saveAndReturn: saveAndReturn ?? {
+      //   keyGenerator: (request) => {
+      //     if (!request.yar.id) {
+      //       throw new Error('No session ID found')
+      //     }
 
-          const state = (request.params.state as string) || ''
-          const slug = (request.params.slug as string) || ''
-          return `${request.yar.id}:${state}:${slug}:`
-        },
+      //     const state = (request.params.state as string) || ''
+      //     const slug = (request.params.slug as string) || ''
+      //     return `${request.yar.id}:${state}:${slug}:`
+      //   },
 
-        sessionHydrator: (request) => {
-          let state = {}
+      //   sessionHydrator: (request) => {
+      //     let state = {}
 
-          if (request.params.slug === 'save-and-return-demo') {
-            logger.info("Loading session for 'save-and-return-demo'")
-            state = {
-              applicantFirstName: 'Enrique',
-              applicantLastName: 'Chase',
-              applicantEmail: 'e.chase@fictional.defra.gov.uk',
-              dateOfBirth__day: 1,
-              dateOfBirth__month: 1,
-              dateOfBirth__year: 1990,
-              address__addressLine1: '10 Downing Street',
-              address__addressLine2: '',
-              address__town: 'London',
-              address__county: '',
-              address__postcode: 'SW1A 2AA',
-              address__country: 'United Kingdom'
-            }
-          }
+      //     if (request.params.slug === 'save-and-return-demo') {
+      //       logger.info("Loading session for 'save-and-return-demo'")
+      //       state = {
+      //         applicantFirstName: 'Enrique',
+      //         applicantLastName: 'Chase',
+      //         applicantEmail: 'e.chase@fictional.defra.gov.uk',
+      //         dateOfBirth__day: 1,
+      //         dateOfBirth__month: 1,
+      //         dateOfBirth__year: 1990,
+      //         address__addressLine1: '10 Downing Street',
+      //         address__addressLine2: '',
+      //         address__town: 'London',
+      //         address__county: '',
+      //         address__postcode: 'SW1A 2AA',
+      //         address__country: 'United Kingdom'
+      //       }
+      //     }
 
-          return Promise.resolve(state)
-        },
+      //     return Promise.resolve(state)
+      //   },
 
-        sessionPersister: (state, _request) => {
-          logger.info(
-            'Session persister called, but not implemented in devtool mode',
-            state
-          )
+      //   sessionPersister: (state, _request) => {
+      //     logger.info(
+      //       'Session persister called, but not implemented in devtool mode',
+      //       state
+      //     )
 
-          return Promise.resolve()
-        }
-      }
+      //     return Promise.resolve()
+      //   }
+      // }
     }
   }
 }
