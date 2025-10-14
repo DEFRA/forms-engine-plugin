e8432490ec6e40ba1b9386133dfe8d48158ac620
David Stone <davidjamesstone@gmail.com>
2025-10-14 10:43:46 +0100

    Update postcode lookup plugin to us external actions

--- src/server/plugins/postcode-lookup/routes/index.js
+++ src/server/plugins/postcode-lookup/routes/index.js
@@ -1,21 +1,14 @@
-import { ComponentType, hasComponentsEvenIfNoNext } from '@defra/forms-model'
 import Boom from '@hapi/boom'
 import { StatusCodes } from 'http-status-codes'
 import Joi from 'joi'
 
-import { FORM_PREFIX } from '~/src/server/constants.js'
-import {
-  checkFormStatus,
-  getCacheService
-} from '~/src/server/plugins/engine/helpers.js'
+import { getCacheService } from '~/src/server/plugins/engine/helpers.js'
 import {
   JOURNEY_BASE_URL,
   detailsPayloadSchema,
   detailsViewModel,
-  getKey,
   manualPayloadSchema,
   manualViewModel,
-  paramsSchema,
   selectPayloadSchema,
   selectViewModel,
   stepSchema,
@@ -26,41 +19,20 @@ import * as service from '~/src/server/plugins/postcode-lookup/service.js'
 const viewName = 'postcode-lookup-details'
 
 /**
- * Get the details of the source form elements associated with this journey
+ * Get the session state associated with this journey
  * @param {PostcodeLookupRequest} request
  */
-function getJourneyDetails(request) {
-  const { app, params } = request
-  const { model } = app
-  const { path, componentName } = params
+function getSessionState(request) {
+  /**
+   * @type {PostcodeLookupSessionData | undefined}
+   */
+  const data = request.yar.get(JOURNEY_BASE_URL)
 
-  if (!model) {
-    throw Boom.notFound(`No model found for ${path}`)
+  if (!data) {
+    throw Boom.notFound(`No data found for ${JOURNEY_BASE_URL}`)
   }
 
-  const { isPreview, state: status } = checkFormStatus(params)
-  const title = model.name
-  const page = model.pageDefMap.get(`/${path}`)
-
-  if (!page) {
-    throw Boom.notFound(`No page found for ${path}`)
-  }
-
-  const component = hasComponentsEvenIfNoNext(page)
-    ? page.components.find((c) => c.name === componentName)
-    : undefined
-
-  if (!component) {
-    throw Boom.notFound(`No component found for name ${componentName}`)
-  }
-
-  if (component.type !== ComponentType.UkAddressField) {
-    throw Boom.internal(
-      `Invalid component type, expected UkAddressFieldComponent got ${component.type}`
-    )
-  }
-
-  return { model, title, page, component, isPreview, status }
+  return data
 }
 
 /**
@@ -95,67 +67,59 @@ async function updateComponentState(request, componentName, address) {
 }
 
 /**
- * Gets the postcode lookup routes
- * @param {RouteOptions<PostcodeLookupRequestRefs>} getRouteOptions - hapi route options
- * @param {PostcodeLookupConfiguration} options - ordnance survey api key
+ * Initialises and dispatches the request to the postcode lookup journey
+ * @param {FormRequestPayload} request - the source page
+ * @param {FormResponseToolkit} h - the source page
+ * @param {PostcodeLookupDispatchData} initial - the source data
  */
-export function getRoutes(getRouteOptions, options) {
-  return [getRoute(getRouteOptions), postRoute(getRouteOptions, options)]
+export function dispatch(request, h, initial) {
+  /**
+   * @type {PostcodeLookupSessionData}
+   */
+  const data = {
+    initial,
+    details: { postcodeQuery: '', buildingNameQuery: '' }
+  }
+
+  request.yar.set(JOURNEY_BASE_URL, data)
+
+  const query = initial.step ? `?step=${initial.step}` : ''
+
+  return h.redirect(`${JOURNEY_BASE_URL}${query}`).code(StatusCodes.SEE_OTHER)
 }
 
 /**
- * @param {RouteOptions<PostcodeLookupRequestRefs>} getRouteOptions
+ * Gets the postcode lookup routes
+ * @param {PostcodeLookupConfiguration} options - ordnance survey api key
+ */
+export function getRoutes(options) {
+  return [getRoute(), postRoute(options)]
+}
+
+/**
  * @returns {ServerRoute<PostcodeLookupGetRequestRefs>}
  */
-function getRoute(getRouteOptions) {
+function getRoute() {
   return {
     method: 'GET',
-    path: `${JOURNEY_BASE_URL}/{slug}/{path}/{componentName}/{state?}`,
+    path: JOURNEY_BASE_URL,
     handler(request, h) {
-      const { params, query } = request
-      const { slug, state: status } = params
-      const { step, clear } = query
-      const { title, page, component } = getJourneyDetails(request)
+      const { query } = request
+      const { step } = query
+      const session = getSessionState(request)
 
-      /**
-       * Get the previous details from session
-       * @type {PostcodeLookupSessionState | undefined}
-       */
-      let previous
-
-      if (clear) {
-        /**
-         * @type {PostcodeLookupSessionState}
-         */
-        const state = {
-          query,
-          details: undefined
-        }
-
-        request.yar.set(getKey(slug, status), state)
-      } else {
-        previous = request.yar.get(getKey(slug, status))
-      }
-
-      const data = { slug, page, title, component, status }
       const model =
         step === steps.manual
-          ? manualViewModel(data)
-          : detailsViewModel(data, previous?.details)
+          ? manualViewModel(session)
+          : detailsViewModel(session)
 
       return h.view(viewName, model)
     },
-    // @ts-expect-error - Request typing
     options: {
-      ...getRouteOptions,
       validate: {
-        params: paramsSchema,
         query: Joi.object()
           .keys({
-            step: Joi.string().allow(steps.details, steps.manual).optional(),
-            clear: Joi.boolean().optional(),
-            returnUrl: Joi.string().optional(),
-            force: Joi.boolean().optional()
+            step: Joi.string().allow(steps.details, steps.manual).optional()
           })
           .optional()
       }
@@ -164,14 +128,13 @@ function getRoute(getRouteOptions) {
 }
 
 /**
- * @param {RouteOptions<PostcodeLookupRequestRefs>} getRouteOptions
  * @param {PostcodeLookupConfiguration} options
  * @returns {ServerRoute<PostcodeLookupPostRequestRefs>}
  */
-function postRoute(getRouteOptions, options) {
+function postRoute(options) {
   return {
     method: 'POST',
-    path: `${JOURNEY_BASE_URL}/{slug}/{path}/{componentName}/{state?}`,
+    path: JOURNEY_BASE_URL,
     async handler(request, h) {
       const { payload } = request
       const { step } = payload
@@ -184,17 +147,14 @@ function postRoute(getRouteOptions, options) {
           return selectPostHandler(request, h, options)
         }
         case steps.manual: {
-          return manualPostHandler(request, h, options)
+          return manualPostHandler(request, h)
         }
         default:
           throw Boom.badRequest(`Invalid step ${step}`)
       }
     },
-    // @ts-expect-error - Request typing
     options: {
-      ...getRouteOptions,
       validate: {
-        params: paramsSchema,
         payload: Joi.object()
           .keys({
             step: stepSchema
@@ -212,33 +172,26 @@ function postRoute(getRouteOptions, options) {
  * @param {PostcodeLookupConfiguration} options
  */
 async function detailsPostHandler(request, h, options) {
-  const { params, payload } = request
-  const { slug, state: status } = params
-  const { title, page, component } = getJourneyDetails(request)
+  const { payload } = request
+  const session = getSessionState(request)
   const { ordnanceSurveyApiKey: apiKey } = options
   const { value: details, error } = detailsPayloadSchema.validate(payload)
 
-  let data, model
+  let model
 
   if (error) {
-    data = { slug, title, page, component, status }
-    model = detailsViewModel(data, details, error)
+    model = detailsViewModel(session, details, error)
 
     return h.view(viewName, model)
   }
-  data = { slug, page, component, details, status, apiKey }
-  model = await selectViewModel(data)
 
-  const key = getKey(slug, status)
+  const { postcodeQuery, buildingNameQuery } = details
+  session.details = { postcodeQuery, buildingNameQuery }
 
-  /**
-   * Get the previous details from session
-   * @type {PostcodeLookupSessionState | undefined}
-   */
-  const previous = request.yar.get(key)
+  // Store the updated session
+  request.yar.set(JOURNEY_BASE_URL, session)
 
-  // Store the new details in session
-  request.yar.set(key, previous ? { ...previous, details } : { details })
+  model = await selectViewModel({ session, apiKey })
 
   return h.view(viewName, model)
 }
@@ -250,17 +203,13 @@ async function detailsPostHandler(request, h, options) {
  * @param {PostcodeLookupConfiguration} options
  */
 async function selectPostHandler(request, h, options) {
-  const { params, payload } = request
-  const { slug, path, componentName, state: status } = params
-  const { page, component } = getJourneyDetails(request)
+  const { payload } = request
+  const session = getSessionState(request)
   const { ordnanceSurveyApiKey: apiKey } = options
   const { value: select, error } = selectPayloadSchema.validate(payload)
 
   if (error) {
-    const { postcodeQuery, buildingNameQuery } = select
-    const details = { postcodeQuery, buildingNameQuery }
-    const data = { slug, page, component, details, status, apiKey }
-    const model = await selectViewModel(data, select, error)
+    const model = await selectViewModel({ session, apiKey }, select, error)
 
     return h.view(viewName, model)
   }
@@ -272,91 +221,41 @@ async function selectPostHandler(request, h, options) {
     throw Boom.internal(`UPRN ${property} not found`)
   }
 
+  const { componentName, sourceUrl } = session.initial
   await updateComponentState(request, componentName, property)
 
   // Redirect back to the source form page
-  const key = getKey(slug, status)
-
-  /**
-   * Get the previous details from session
-   * @type {PostcodeLookupSessionState | undefined}
-   */
-  const previous = request.yar.get(key)
-  const url = new URL(
-    `${FORM_PREFIX}/${slug}/${path}`,
-    options.enginePluginOptions.baseUrl
-  )
-
-  if (previous?.query) {
-    const query = previous.query
-
-    if (query.returnUrl) {
-      url.searchParams.append('returnUrl', query.returnUrl)
-    }
-
-    if (query.force !== undefined) {
-      url.searchParams.append('force', `${query.force}`)
-    }
-  }
-
-  // Redirect back to the source form page
-  return h.redirect(url.toString()).code(StatusCodes.SEE_OTHER)
+  return h.redirect(sourceUrl).code(StatusCodes.SEE_OTHER)
 }
 
 /**
  * Post handler for the manual step
  * @param {PostcodeLookupPostRequest} request
  * @param {ResponseToolkit<PostcodeLookupPostRequestRefs>} h
- * @param {PostcodeLookupConfiguration} options
  */
-async function manualPostHandler(request, h, options) {
-  const { params, payload } = request
-  const { slug, path, componentName, state: status } = params
-  const { title, page, component } = getJourneyDetails(request)
+async function manualPostHandler(request, h) {
+  const { payload } = request
+  const session = getSessionState(request)
 
   const { value: manual, error } = manualPayloadSchema.validate(payload, {
     abortEarly: false
   })
 
   if (error) {
-    const data = { slug, title, page, component, status }
-    const model = manualViewModel(data, manual, error)
+    const model = manualViewModel(session, manual, error)
 
     return h.view(viewName, model)
   }
 
+  const { componentName, sourceUrl } = session.initial
   await updateComponentState(request, componentName, manual)
 
   // Redirect back to the source form page
-  const key = getKey(slug, status)
-
-  /**
-   * Get the previous details from session
-   * @type {PostcodeLookupSessionState | undefined}
-   */
-  const previous = request.yar.get(key)
-  const url = new URL(
-    `${FORM_PREFIX}/${slug}/${path}`,
-    options.enginePluginOptions.baseUrl
-  )
-
-  if (previous?.query) {
-    const query = previous.query
-
-    if (query.returnUrl) {
-      url.searchParams.append('returnUrl', query.returnUrl)
-    }
-
-    if (query.force !== undefined) {
-      url.searchParams.append('force', `${query.force}`)
-    }
-  }
-
-  // Redirect back to the source form page
-  return h.redirect(url.toString()).code(StatusCodes.SEE_OTHER)
+  return h.redirect(sourceUrl).code(StatusCodes.SEE_OTHER)
 }
 
 /**
- * @import { ResponseToolkit, RouteOptions, ServerRoute } from '@hapi/hapi'
- * @import { PostcodeLookupManualPayload, Address, PostcodeLookupGetRequestRefs, PostcodeLookupPostRequestRefs, PostcodeLookupRequest, PostcodeLookupRequestRefs, PostcodeLookupPostRequest, PostcodeLookupSessionState, PostcodeLookupConfiguration } from '~/src/server/plugins/postcode-lookup/types.js'
+ * @import { ResponseToolkit, ServerRoute } from '@hapi/hapi'
+ * @import { PostcodeLookupManualPayload, Address, PostcodeLookupGetRequestRefs, PostcodeLookupPostRequestRefs, PostcodeLookupRequest, PostcodeLookupPostRequest, PostcodeLookupConfiguration, PostcodeLookupDispatchData, PostcodeLookupSessionData } from '~/src/server/plugins/postcode-lookup/types.js'
+ * @import { FormRequestPayload, FormResponseToolkit } from '~/src/server/routes/types.js'
  */
