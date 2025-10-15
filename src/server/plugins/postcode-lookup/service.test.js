4d006aaef492d6605778befb7506742fa4df5a0e
David Stone <davidjamesstone@gmail.com>
2025-10-15 12:36:58 +0100

    Update postcode lookup tests

--- src/server/plugins/postcode-lookup/service.test.js
+++ src/server/plugins/postcode-lookup/service.test.js
@@ -20,18 +20,18 @@ describe('Postcode lookup service', () => {
         error: undefined
       })
 
-      const results = await service.searchByPostcode('cw8 2at', 'apikey')
+      const results = await service.searchByPostcode('NW1 6XE', 'apikey')
 
       expect(results).toHaveLength(10)
       expect(results.at(0)).toEqual({
-        address: 'FOREST DENE, FOREST HILL, HARTFORD, NORTHWICH, CW8 2AT',
-        addressLine1: 'Forest Dene',
-        addressLine2: 'Forest Hill, Hartford',
+        address: "EMILIA'S CRAFTED PASTA, 215, BAKER STREET, LONDON, NW1 6XE",
+        addressLine1: "Emilia's Crafted Pasta 215",
+        addressLine2: 'Baker Street',
         county: '',
-        formatted: 'Forest Dene, Forest Hill, Hartford, Northwich, CW8 2AT',
-        postcode: 'CW8 2AT',
-        town: 'Northwich',
-        uprn: '200003232010'
+        formatted: "Emilia's Crafted Pasta 215, Baker Street, London, NW1 6XE",
+        postcode: 'NW1 6XE',
+        town: 'London',
+        uprn: '10033619968'
       })
     })
 
@@ -45,7 +45,7 @@ describe('Postcode lookup service', () => {
         error: new Error('Unknown error')
       })
 
-      const results = await service.searchByPostcode('cw8 2at', 'apikey')
+      const results = await service.searchByPostcode('NW1 6XE', 'apikey')
 
       expect(results).toHaveLength(0)
       expect(results).toEqual([])
@@ -80,7 +80,7 @@ describe('Postcode lookup service', () => {
         error: undefined
       })
 
-      const results = await service.searchByPostcode('cw8 2at', 'apikey')
+      const results = await service.searchByPostcode('NW1 6XE', 'apikey')
 
       expect(results).toHaveLength(0)
       expect(results).toEqual([])
@@ -98,18 +98,18 @@ describe('Postcode lookup service', () => {
         error: undefined
       })
 
-      const results = await service.searchByUPRN('200003232010', 'apikey')
+      const results = await service.searchByUPRN('100023071949', 'apikey')
 
       expect(results).toHaveLength(1)
       expect(results.at(0)).toEqual({
-        address: 'FOREST DENE, FOREST HILL, HARTFORD, NORTHWICH, CW8 2AT',
-        addressLine1: 'Forest Dene',
-        addressLine2: 'Forest Hill, Hartford',
+        address: 'SHERLOCK HOLMES MUSEUM, 221B, BAKER STREET, LONDON, NW1 6XE',
+        addressLine1: 'Sherlock Holmes Museum 221b',
+        addressLine2: 'Baker Street',
         county: '',
-        formatted: 'Forest Dene, Forest Hill, Hartford, Northwich, CW8 2AT',
-        postcode: 'CW8 2AT',
-        town: 'Northwich',
-        uprn: '200003232010'
+        formatted: 'Sherlock Holmes Museum 221b, Baker Street, London, NW1 6XE',
+        postcode: 'NW1 6XE',
+        town: 'London',
+        uprn: '100023071949'
       })
     })
   })
@@ -126,20 +126,20 @@ describe('Postcode lookup service', () => {
       })
 
       const results = await service.searchByQuery(
-        'forest dene northwich',
+        'Prime minister downing',
         'apikey'
       )
 
       expect(results).toHaveLength(5)
       expect(results.at(0)).toEqual({
-        address: 'FOREST DENE, FOREST HILL, HARTFORD, NORTHWICH, CW8 2AT',
-        addressLine1: 'Forest Dene',
-        addressLine2: 'Forest Hill, Hartford',
+        address: 'BAKER STREET COTTAGE, BAKER STREET, FROME, BA11 3BL',
+        addressLine1: 'Baker Street Cottage',
+        addressLine2: 'Baker Street',
+        town: 'Frome',
         county: '',
-        formatted: 'Forest Dene, Forest Hill, Hartford, Northwich, CW8 2AT',
-        postcode: 'CW8 2AT',
-        town: 'Northwich',
-        uprn: '200003232010'
+        formatted: 'Baker Street Cottage, Baker Street, Frome, BA11 3BL',
+        postcode: 'BA11 3BL',
+        uprn: '250034655'
       })
     })
   })
@@ -155,18 +155,18 @@ describe('Postcode lookup service', () => {
         error: undefined
       })
 
-      const results = await service.search('cw8 2at', 'Dene', 'apikey')
+      const results = await service.search('NW1 6XE', 'Emilia', 'apikey')
 
       expect(results).toHaveLength(1)
       expect(results.at(0)).toEqual({
-        address: 'FOREST DENE, FOREST HILL, HARTFORD, NORTHWICH, CW8 2AT',
-        addressLine1: 'Forest Dene',
-        addressLine2: 'Forest Hill, Hartford',
+        address: "EMILIA'S CRAFTED PASTA, 215, BAKER STREET, LONDON, NW1 6XE",
+        addressLine1: "Emilia's Crafted Pasta 215",
+        addressLine2: 'Baker Street',
         county: '',
-        formatted: 'Forest Dene, Forest Hill, Hartford, Northwich, CW8 2AT',
-        postcode: 'CW8 2AT',
-        town: 'Northwich',
-        uprn: '200003232010'
+        formatted: "Emilia's Crafted Pasta 215, Baker Street, London, NW1 6XE",
+        postcode: 'NW1 6XE',
+        town: 'London',
+        uprn: '10033619968'
       })
     })
   })
