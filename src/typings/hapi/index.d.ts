c7e7ec3360907d67099e90a26186f8dc94fd2e85
Alex Luckett <alexgluckett@gmail.com>
2025-11-17 17:09:48 +0000

    Use new @hapi/yar typing system

--- src/typings/hapi/index.d.ts
+++ src/typings/hapi/index.d.ts
@@ -4,12 +4,25 @@ import { type Plugin } from '@hapi/hapi'
 import { type ServerYar, type Yar } from '@hapi/yar'
 import { type Logger } from 'pino'
 
+import {
+  type EXTERNAL_STATE_APPENDAGE,
+  type EXTERNAL_STATE_PAYLOAD
+} from '~/src/server/constants.js'
 import { type FormModel } from '~/src/server/plugins/engine/models/index.js'
 import {
   type AnyFormRequest,
+  type FormSubmissionError,
   type PluginOptions
 } from '~/src/server/plugins/engine/types.ts'
 import { type CacheService } from '~/src/server/services/index.js'
+
+declare module '@hapi/yar' {
+  interface YarFlashes {
+    [EXTERNAL_STATE_APPENDAGE]: object
+    [EXTERNAL_STATE_PAYLOAD]: object
+    [key: string]: { errors: FormSubmissionError[] }
+  }
+}
 
 declare module '@hapi/hapi' {
   // Here we are decorating Hapi interface types with
