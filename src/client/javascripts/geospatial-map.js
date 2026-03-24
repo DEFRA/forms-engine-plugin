916330b5b6b11c31e41021b8f104318c9d37d5a6
David Stone <davidjamesstone@gmail.com>
2026-03-24 09:10:59 +0000

    SVG constants

--- src/client/javascripts/geospatial-map.js
+++ src/client/javascripts/geospatial-map.js
@@ -54,6 +54,13 @@ const typeDescriptions = {
   Polygon: 'Shape'
 }
 
+const POINT_SVG =
+  '<path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" /><circle cx="12" cy="10" r="3" />'
+const POLYGON_SVG =
+  '<path d="M19.5 7v10M4.5 7v10M7 19.5h10M7 4.5h10"/><path d="M22 18v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zm0-15v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zM7 18v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zM7 3v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1z"/>'
+const LINE_SVG =
+  '<path d="M5.706 16.294L16.294 5.706"/><path d="M21 2v3c0 .549-.451 1-1 1h-3c-.549 0-1-.451-1-1V2c0-.549.451-1 1-1h3c.549 0 1 .451 1 1zM6 17v3c0 .549-.451 1-1 1H2c-.549 0-1-.451-1-1v-3c0-.549.451-1 1-1h3c.549 0 1 .451 1 1z"/>'
+
 /**
  * Extract and parses the GeoJSON from the textarea
  * @param {HTMLTextAreaElement} geospatialInput - the textarea containing the geojson
@@ -532,8 +539,7 @@ function onMapReadyFactory(context) {
     map.addButton('btnAddPoint', {
       variant: 'tertiary',
       label: 'Add point',
-      iconSvgContent:
-        '<path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" /><circle cx="12" cy="10" r="3" /> ',
+      iconSvgContent: POINT_SVG,
       onClick: () => {
         resetActiveFeature()
         toggleActionButtons(true)
@@ -547,8 +553,7 @@ function onMapReadyFactory(context) {
     map.addButton('btnAddPolygon', {
       variant: 'tertiary',
       label: 'Add shape',
-      iconSvgContent:
-        '<path d="M19.5 7v10M4.5 7v10M7 19.5h10M7 4.5h10"/><path d="M22 18v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zm0-15v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zM7 18v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zM7 3v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1z"/>',
+      iconSvgContent: POLYGON_SVG,
       onClick: () => {
         resetActiveFeature()
         toggleActionButtons(true)
@@ -562,8 +567,7 @@ function onMapReadyFactory(context) {
     map.addButton('btnAddLine', {
       variant: 'tertiary',
       label: 'Add line',
-      iconSvgContent:
-        '<path d="M5.706 16.294L16.294 5.706"/><path d="M21 2v3c0 .549-.451 1-1 1h-3c-.549 0-1-.451-1-1V2c0-.549.451-1 1-1h3c.549 0 1 .451 1 1zM6 17v3c0 .549-.451 1-1 1H2c-.549 0-1-.451-1-1v-3c0-.549.451-1 1-1h3c.549 0 1 .451 1 1z"/>',
+      iconSvgContent: LINE_SVG,
       onClick: () => {
         resetActiveFeature()
         toggleActionButtons(true)
