59be9bb439fb9f52e5efd5c8b618d3acf12aae77
David Stone <davidjamesstone@gmail.com>
2026-04-08 09:11:49 +0100

    Spelling typo fix

--- src/client/javascripts/geospatial-map.js
+++ src/client/javascripts/geospatial-map.js
@@ -245,7 +245,7 @@ function createFeatureHTML(feature, index, mapId, readonly) {
           <dd class="govuk-summary-list__value">${typeDescriptions[feature.geometry.type]}</dd>
         </div>
         <div class="govuk-summary-list__row">
-          <dt class="govuk-summary-list__key">Center grid reference</dt>
+          <dt class="govuk-summary-list__key">Centre grid reference</dt>
           <dd class="govuk-summary-list__value">${feature.properties.centroidGridReference}</dd>
         </div>
         <div class="govuk-summary-list__row">
