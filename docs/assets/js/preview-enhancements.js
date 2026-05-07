fccf435492fc8edc7f0664a1a79acc55776b46e6
Alex Luckett <alex@nodepoint.co.uk>
2026-05-07 16:59:20 +0100

    fix: correct relative path to autocomplete.js (docs/assets/js is 3 levels deep)

--- docs/assets/js/preview-enhancements.js
+++ docs/assets/js/preview-enhancements.js
@@ -2,7 +2,7 @@
 // Re-uses the application's own autocomplete initialisation rather than duplicating it.
 
 // eslint-disable-next-line no-restricted-imports
-import { initAllAutocomplete } from '../../src/client/javascripts/autocomplete.js'
+import { initAllAutocomplete } from '../../../src/client/javascripts/autocomplete.js'
 
 export function onRouteDidUpdate() {
   initAllAutocomplete()
