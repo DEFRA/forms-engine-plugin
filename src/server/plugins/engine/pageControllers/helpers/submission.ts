f61525782c0e59ef810df76aa6e58ec155738b1f
David Stone <davidjamesstone@gmail.com>
2026-06-24 15:54:45 +0100

    Bug/optional geospatial component submit data (#434)
    * For optional geospatial components, send an empty string on submit if undefined
    
    * For optional geospatial components in repeaters, send an empty string on submit if undefined
    
    * Add geospatial optional tests sends empty string on submit

--- src/server/plugins/engine/pageControllers/helpers/submission.ts
+++ src/server/plugins/engine/pageControllers/helpers/submission.ts
@@ -36,10 +36,11 @@ export function buildMainRecords(items: DetailItem[]): SubmitRecord[] {
     } else if (item.field instanceof GeospatialField) {
       // Stringify of GeoJSON is done here rather than inside `getContextValueFromState`
       // so we don't incur the overhead of JSON.stringify on every request when building context
+      const value = item.field.getFormValueFromState(item.state)
       records.push({
         name: item.name,
         title: item.label,
-        value: JSON.stringify(item.field.getFormValueFromState(item.state))
+        value: value === undefined ? '' : JSON.stringify(value)
       })
     } else {
       records.push({
@@ -109,18 +110,24 @@ export function buildRepeaterRecords(
       name: item.name,
       title: item.label,
       value: item.subItems.map((detailItems) =>
-        detailItems.map((subItem) => ({
-          name: subItem.name,
-          title: subItem.label,
-          value:
+        detailItems.map((subItem) => {
+          let value
+
+          if (subItem.field instanceof GeospatialField) {
             // Stringify of GeoJSON is done here rather than inside `getContextValueFromState`
             // so we don't incur the overhead of JSON.stringify on every request when building context
-            subItem.field instanceof GeospatialField
-              ? JSON.stringify(
-                  subItem.field.getFormValueFromState(subItem.state)
-                )
-              : getAnswer(subItem.field, subItem.state, { format: 'data' })
-        }))
+            const formValue = subItem.field.getFormValueFromState(subItem.state)
+            value = formValue === undefined ? '' : JSON.stringify(formValue)
+          } else {
+            value = getAnswer(subItem.field, subItem.state, { format: 'data' })
+          }
+
+          return {
+            name: subItem.name,
+            title: subItem.label,
+            value
+          }
+        })
       )
     }))
 }
