2f699ed367b2f9866d078198deef15511937a6a9
David Stone <davidjamesstone@gmail.com>
2026-08-05 10:07:08 +0100

    Auto-submit file uploads to CDP

--- src/client/javascripts/file-upload.js
+++ src/client/javascripts/file-upload.js
@@ -425,6 +425,7 @@ function initUpload() {
     if (fileInput.files && fileInput.files.length > 0) {
       selectedFiles = Array.from(fileInput.files)
     }
+    uploadButton.click()
   })
 
   uploadButton.addEventListener('click', (event) => {
